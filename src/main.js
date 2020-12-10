cb05c0c994df2438c9f5c5027278e966e3013b50
林水溶 <shuirong1997@icloud.com>
2020-12-10 15:13:06 +0800

    feat: 增加.env配置文件读取功能；走通备份保存流程；

--- src/main.js
+++ src/main.js
@@ -1,63 +1,103 @@
+const dotenv = require('dotenv-safe');
 const WebSocketClient = require('websocket').client;
 const client = new WebSocketClient();
 const urlParse = require('url').parse;
 const parse = require('./parse');
 const makeTelegraph = require('./makeTelegraph');
-const { JSDOM }  = require('jsdom');
+const { JSDOM } = require('jsdom');
 
-const main = async () => {
-    const { title, content } = await parse(
-        'https://www.douban.com/note/785911942/'
-    );
-    // console.log('result is: ', title, content);
+let domain = '';
+let telegraphToken = '';
+let mastodonToken = '';
+let whiteList = [];
 
-    const { telegraphLink, isLong, pages, push } =  await makeTelegraph({
-        title,
-        author_url: 'https://telegra.ph/api#createAccount',
-        access_token: '2cec0afb49ed4706164bea7e795f45ff71160e68d6e01670477c0762d4b8'
-    },content)
+const init = () => {
+    dotenv.config({
+        allowEmptyValues: true,
+        path: path.join(__dirname, '../.env'),
+        sample: path.join(__dirname, '../.env.example'),
+    });
+    domain = process.env.DOMAIN;
+    telegraphToken = process.env.TELEGRAPH_TOKEN;
+    mastodonToken = process.env.MASTODON_TOKEN;
 
-    console.log(telegraphLink,isLong,pages,push)
+    // 白名单转换成数组
+    if (process.env.WHITELIST) {
+        whiteList = process.env.WHITELIST.split(' ');
+    }
 };
 
-// main();
+const archive = async (link) => {
+    const { title, content } = await parse(link);
 
-client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
-    process.exit(1)
-});
+    const { telegraphLink, isLong, pages, push } = await makeTelegraph(
+        {
+            title,
+            author_url: 'https://telegra.ph/api#createAccount',
+            access_token: telegraphToken,
+        },
+        content
+    );
+    console.log(telegraphLink, isLong, pages, push);
+    return telegraphLink;
+};
 
-client.on('connect', function(connection) {
-    console.log("WS Conntection Success!")
-    connection.on('error', function(error) {
-        console.log("Connection Error: " + error.toString());
-        process.exit(1)
+// 接收实例数据流
+const listen = () => {
+    client.on('connectFailed', function (error) {
+        console.log('Connect Error: ' + error.toString());
+        process.exit(1);
     });
-    connection.on('close', function() {
-        console.log('echo-protocol Connection Closed');
-        process.exit(1)
+
+    client.on('connect', function (connection) {
+        console.log('WS Conntection Success!');
+        connection.on('error', function (error) {
+            console.log('Connection Error: ' + error.toString());
+            process.exit(1);
+        });
+        connection.on('close', function () {
+            console.log('echo-protocol Connection Closed');
+            process.exit(1);
+        });
+        connection.on('message', function (message) {
+            if (message.type === 'utf8') {
+                const data = JSON.parse(message.utf8Data);
+                if (data.event !== 'update') return;
+                const status = JSON.parse(data.payload);
+
+                // if(status.account.username === 'beifen') return
+
+                const { window } = new JSDOM(
+                    `<!DOCTYPE html>${status.content}`
+                );
+                const $ = require('jquery')(window);
+
+                $('a').each(async () => {
+                    const link = $(this).text();
+                    const { host, hostname } = urlParse(link);
+                    // 如果用户发出的链接是当前实例的 或者 域名不在白名单的，不备份
+                    if (hostname === domain || !whiteList.includes(hostname)) {
+                        return;
+                    }
+                    console.log('link host,hostname', host, hostname);
+                    const telegraphLink = await archive(link);
+                    console.log(
+                        'telegraphLink2',
+                        telegraphLink,
+                        ' ',
+                        status.id
+                    );
+                });
+            }
+        });
     });
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            const data = JSON.parse(message.utf8Data)
-            if(data.event !== 'update') return
 
-            const status = JSON.parse(data.payload)
-            const {host,hostname} = urlParse(status.account.url)
-            console.log("toot host,hostname: ",host,hostname)
-            // if(status.account.username === 'beifen') return
+    client.connect(
+        'wss://' + domain + '/api/v1/streaming/?stream=public:local'
+    );
+};
 
-            const { window } = new JSDOM(`<!DOCTYPE html>${status.content}`);
-            const $ = require( "jquery" )( window );
+init();
+main();
 
-            $('a').each(()=>{
-                const link = $(this).text();
-                const {host,hostname} = urlParse(link)
-                console.log("link",host,hostname)
-            })
-        }
-    });
-});
-
-client.connect("wss://" + "alive.bar" + "/api/v1/streaming/?stream=public:local");
-console.log(2)
+const main = async () => {};
